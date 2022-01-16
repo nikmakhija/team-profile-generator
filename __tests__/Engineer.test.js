489a003e907f4c339c7f9627791a94bd096f8af6
Makhija <makhija.1@ad.cob.ohio-state.edu>
2022-01-16 14:45:09 -0500

    Team-Profile-Generator

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,37 +1,19 @@
-const Employee = require("../lib/Employee")
-const Engineer = require("../lib/Engineer")
 const Engineer = require("../lib/Engineer")
 
-test("creates an engineer instance",()=>{
-    const engineer = new Engineer()
-expect(typeof(engineer)).toBe("object")
-})
-
-test("can set name",()=>{
-const name = "nikhil"
-const engineer = new Engineer(name)
-expect(engineer.name).toBe(name)
-})
-
-test("can set id",()=>{
-    const testValue = 100;
-    const engineer = new Engineer("code",testValue)
-    expect(engineer.id).toBe(testValue)
-})
-
-test("can set email",()=>{
-    const email = "nikhilmakhija@hotmail.com"
-    const engineer = new Engineer("code",1,email)
+test("creates an GitHub account",()=>{
+    const testValue = "nikmakhija"
+    const employee = new Engineer("Nikhil", 1, testValue)
+expect(employee.github).toBe("testValue")
 })
 
 test("getrole should return \"Engineer\"",()=>{
-    const testValue = "engineer"
-    const engineer = new Employee("nikhil",1,"nikhilmakhija@hotmail.com")
-    expect(engineer.getRole()).toBe(testValue)
+    const testValue = "Engineer"
+    const employee = new Engineer("nikhil",1,"nikhilmakhija@hotmail.com", "nikmakhija")
+    expect(employee.getRole()).toBe(testValue)
 })
 
-test("getgithub should return \"Engineer\"",()=>{
-    const testValue = "engineer"
-    const engineer = new Engineer("nikhil",1,"nikmakhija")
-    expect(engineer.getGithub()).toBe(testValue)
+test("Can get GitHub username via getGithub()", ()=> {
+    const testValue = "nikmakhija"
+    const employee = new Engineer("nikhil", 1, "nikhilmakhija@hotmail.com", testValue)
+    expect(employee.github()).toBe(testValue)
 })
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,29 +1,20 @@
 const Manager = require("../lib/Employee")
+const employee = require("../lib/Employee")
 
-test("creates an manager instance",()=>{
-    const manager = new Manager()
-expect(typeof(manager)).toBe("object")
-})
-
-test("can set name",()=>{
-const name = "nikhil"
-const manager = new Manager(name)
-expect(manager.name).toBe(name)
-})
-
-test("can set id",()=>{
+test("Can set office number", ()=>{
     const testValue = 100;
-    const manager = new Manager("code",testValue)
-    expect(manager.id).toBe(testValue)
-})
-
-test("can set email",()=>{
-    const email = "nikhilmakhija@hotmail.com"
-    const manager = new Manager(email)
+    const employee = new Manager("nikhil", 1, "nikhilmakhija@hotmail.com", testValue)
+    expect(employee.officeNumber).toBe(testValue)
 })
 
 test("getrole should return \"Manager\"",()=>{
     const testValue = "manager"
-    const employee = new Manager("nikhil",1,"nikhilmakhija@hotmail.com")
-    expect(manager.getRole()).toBe(testValue)
+    const employee = new Manager("nikhil",1,"nikhilmakhija@hotmail.com", 100)
+    expect(employee.getRole()).toBe(testValue)
+})
+
+test("Can get office number()", ()=> {
+    const testValue = 100;
+    const employee = new Manager("nikhil", 1, "nikhilmakhija@hotmail.com", testValue)
+    expect(employee.getOfficeNumber()).toBe(testValue)
 })
